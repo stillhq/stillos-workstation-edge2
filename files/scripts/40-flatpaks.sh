47391883c617cedb1df9c0bd95611897b5008c24
PizzaLovingNerd <cameron@stillhq.io>
2026-01-09 16:02:45 -0800

    moved flatpak script location

---
+++ files/scripts/40-flatpaks.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+
+sudo dnf install -y still-bundler
+
+still-bundler ci-build \
+    org.gnome.Epiphany \
+    com.github.tchx84.Flatseal \
+    de.haeckerfelix.Shortwave \
+    org.gnome.Contacts \
+    ca.edestcroix.Recordbox \
+    org.gnome.Podcasts \
+    org.gnome.SoundRecorder \
+    com.github.maoschanz.drawing \
+    com.belmoussaoui.Authenticator \
+    org.gnome.World.Secrets \
+    io.gitlab.news_flash.NewsFlash \
+    com.rafaelmardojai.Blanket \
+    org.onlyoffice.desktopeditors \
+    app.drey.Warp \
+    -o /path/to/flatpak-bundle.tar.xz \
+    --i-understand-this-will-reset-flatpak
