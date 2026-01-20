77d7e54012070acf382651f14e5dbd4196c1ef1f
PizzaLovingNerd <cameron@stillhq.io>
2026-01-19 19:50:39 -0800

    Update 91-image-info.sh

--- files/scripts/91-image-info.sh
+++ files/scripts/91-image-info.sh
@@ -6,19 +6,19 @@ set -xeuo pipefail
 
 cat << 'EOF' > /etc/os-release
 NAME="stillOS"
-VERSION="10 Snapshot 26m1a (The Bloom)"
+VERSION="10-26m1a"
 ID="stillos"
 ID_LIKE="almalinux rhel centos fedora"
 VERSION_ID="10"
 PLATFORM_ID="platform:el10"
-PRETTY_NAME="stillOS Snapshot 26m1a (The Bloom)"
+PRETTY_NAME="stillOS 10 (Snapshot 26m1a)
 ANSI_COLOR="0;34"
 LOGO="fedora-logo-icon"
 CPE_NAME="cpe:/o:almalinux:almalinux:10::baseos"
-HOME_URL="https://almalinux.org/"
+HOME_URL="https://stillhq.io/"
 DOCUMENTATION_URL="https://wiki.almalinux.org/"
-VENDOR_NAME="AlmaLinux"
-VENDOR_URL="https://almalinux.org/"
+VENDOR_NAME="stillHQ"
+VENDOR_URL="https://stillhq.io/"
 BUG_REPORT_URL="https://bugs.almalinux.org/"
 
 ALMALINUX_MANTISBT_PROJECT="AlmaLinux-10"
